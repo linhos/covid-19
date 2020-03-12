965e45585050f27bba5a686d46efd7239510c35b
cgonzalez@hubescuela.com <cgonzalez@hubescuela.com>
2020-03-12 23:29:02 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Covid from "./components/Covid";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Covid />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Covid.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import Moment from "react-moment";
+
+const Covid = () => {
+  const [confirmed, setConfirmed] = useState("");
+  const [recovered, setRecovered] = useState("");
+  const [deaths, setDeaths] = useState("");
+  const [lastUpdate, setLastUpdate] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+      const data = await fetch(`https://covid19.mathdro.id/api/countries/ES`)
+        .then(res => res.json())
+        .catch(err => setError(err));
+
+      setConfirmed(data.confirmed.value);
+      setRecovered(data.recovered.value);
+      setDeaths(data.deaths.value);
+      setLastUpdate(data.lastUpdate);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <span>
+      {loading && <h1>Cargando ...</h1>}
+      <h1>Casos COVID-19 ESPAÑA</h1>
+      Confirmados: <span className="text-primary">{confirmed}</span>
+      <br />
+      Recuperados: {recovered}
+      <br />
+      Fallecidos: <span className="text-danger">{deaths}</span>
+      <br />
+      Actualizado: <Moment fromNow>{lastUpdate}</Moment>
+    </span>
+  );
+};
+
+export default Covid;
